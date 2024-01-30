f3868f09964cf86932f137a46d3e58a816e454e7
Nikhil Reddy <nikhilreddy3111@gmail.com>
2024-01-30 13:27:46 -0500

    code

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-ec2" {
+  ami                         = "ami-0dfcb1ef8550277af"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  tags = {
+    "Name" = "freestyle-ec2"
+  }
+  user_data = filebase64("${path.module}/ec2-user-data.sh")
+}
+
