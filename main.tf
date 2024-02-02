09867190fb0903b3c357aa1f90e9b9c3ab138356
Musku Nikhil <78166930+MuskuNikhil0191@users.noreply.github.com>
2024-02-02 10:51:42 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-ec2" {
-  ami                         = "ami-0866a04d72a1f5479"
+  ami                         = "ami-05fb0b8c1424f266b"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
   tags = {
