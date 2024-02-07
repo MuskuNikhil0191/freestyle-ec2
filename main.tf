1d163670d180217e756d7ac7664435b493c7b874
Musku Nikhil <78166930+MuskuNikhil0191@users.noreply.github.com>
2024-02-06 21:30:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,19 +19,19 @@ resource "aws_instance" "demo-ec2" {
   tags = {
     "Name" = "freestyle-ec2"
   }
-  user_data = << EOF
+  user_data = <<-EOF
 		#!/bin/bash
-    # Use this for your user data (script from top to bottom)
-    # install httpd (Linux 2 version)
-    yum update -y
-    yum install -y httpd
-    systemctl start httpd
-    systemctl enable httpd
-    echo "<html>
-        <body>
-        <h1>Rohith Sharma</h1>
-        <img src="https://pbs.twimg.com/media/E1GgiDYUUAswk4R.jpg:large" width="400" height="600">
-        </body>
-        </html>" > /var/www/html/index.html
-	EOF
+    		# Use this for your user data (script from top to bottom)
+    		# install httpd (Linux 2 version)
+		yum update -y
+	    	yum install -y httpd
+	    	systemctl start httpd
+		systemctl enable httpd
+    		echo "<html>
+	        <body>
+	        <h1>Rohith Sharma</h1>
+	        <img src="https://pbs.twimg.com/media/E1GgiDYUUAswk4R.jpg:large" width="400" height="600">
+	        </body>
+        	</html>" > /var/www/html/index.html
+		EOF
 }
