aca7eb93d443e45568e82cf363d25b8e06266349
Nikhil Reddy <nikhilreddy3111@gmail.com>
2024-02-06 23:34:26 -0500

    code

--- main.tf
+++ main.tf
@@ -20,18 +20,13 @@ resource "aws_instance" "demo-ec2" {
     "Name" = "freestyle-ec2"
   }
   user_data = <<-EOF
-		#!/bin/bash
-    		# Use this for your user data (script from top to bottom)
-    		# install httpd (Linux 2 version)
-		yum update -y
-	    	yum install -y httpd
-	    	systemctl start httpd
-		systemctl enable httpd
-    		echo "<html>
-	        <body>
-	        <h1>Rohith Sharma</h1>
-	        <img src="https://pbs.twimg.com/media/E1GgiDYUUAswk4R.jpg:large" width="400" height="600">
-	        </body>
-        	</html>" > /var/www/html/index.html
-		EOF
+              #!/bin/bash
+              # Use this for your user data (script from top to bottom)
+              # install httpd (Linux 2 version)
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "Hello from User Data!" > /var/www/html/index.html
+              EOF
 }
